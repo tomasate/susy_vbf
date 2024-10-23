dae8cf2dae7b71adfd167b8e2fe000579f9a7b53
deoache <daniel.ocampoh@udea.edu.co>
2024-10-23 17:14:37 +0000

    update

--- analysis/helpers/__init__.py
+++ analysis/helpers/__init__.py
@@ -1,2 +1,3 @@
+from analysis.helpers.output_directory_builder import get_output_directory
 from analysis.helpers.path_handler import Paths
 paths = Paths(True)
--- analysis/helpers/misc.py
+++ analysis/helpers/misc.py
@@ -1,6 +1,4 @@
 import awkward as ak
-from analysis.helpers.path_handler import Paths
-
 
 def delta_r_mask(first: ak.Array, second: ak.Array, threshold: float) -> ak.Array:
     """
@@ -22,16 +20,3 @@ def delta_r_mask(first: ak.Array, second: ak.Array, threshold: float) -> ak.Arra
     """
     mval = first.metric_table(second)
     return ak.all(mval > threshold, axis=-1)
-
-
-def get_output_directory(args: dict) -> str:
-    """builds output directories for data and metadata. Return output path"""
-    # get processor output path
-    paths = Paths(eval(args["eos"]))
-    processor_output_path = paths.processor_path(
-        processor_name=args["processor"],
-        dataset_year=args["year"],
-        label=args["label"],
-        mkdir=True,
-    )
-    return str(processor_output_path)
