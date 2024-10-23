1b964e55784bc154936ce6d687a54d070fe05ff0
deoache <daniel.ocampoh@udea.edu.co>
2024-10-23 15:27:02 +0000

    add submit scripts

---
+++ submit.py
@@ -0,0 +1,63 @@
+import json
+import time
+import argparse
+from coffea import processor
+from analysis.processors.ztojets import ZToJets
+
+
+def main(args):
+    processors = {
+        "ztojets": ZToJets(year=args.year),
+    }
+    t0 = time.monotonic()
+    out = processor.run_uproot_job(
+        args.partition_fileset,
+        treename="Events",
+        processor_instance=processors[args.processor],
+        executor=processor.futures_executor,
+        executor_args={"schema": processor.NanoAODSchema, "workers": 4},
+    )
+    exec_time = format_timespan(time.monotonic() - t0)
+
+    print(f"Execution time: {exec_time}")
+    with open(f"{args.output_path}/{args.dataset_key}.pkl", "wb") as handle:
+        pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--processor",
+        dest="processor",
+        type=str,
+        default="ztojets",
+        help="processor to be used {ztojets}",
+    )
+    parser.add_argument(
+        "--dataset_key",
+        dest="dataset_key",
+        type=str,
+        default="",
+        help="dataset_key",
+    )
+    parser.add_argument(
+        "--partition_fileset",
+        dest="partition_fileset",
+        type=json.loads,
+        help="partition_fileset needed to preprocess a fileset",
+    )
+    parser.add_argument(
+        "--year",
+        dest="year",
+        type=str,
+        default="",
+        help="year of the data {2016preVFP, 2016postVFP, 2017, 2018}",
+    )
+    parser.add_argument(
+        "--output_path",
+        dest="output_path",
+        type=str,
+        help="output path",
+    )
+    args = parser.parse_args()
+    main(args)
---
+++ submit_condor.py
@@ -0,0 +1,124 @@
+import json
+import argparse
+from pathlib import Path
+from condor import submit_condor
+from analysis.fileset import divide_list
+from analysis.helpers.path_handler import Paths
+
+
+def get_output_directories(args: dict) -> str:
+    """builds output directories for data and metadata. Return output path"""
+    # get processor output path
+    paths = Paths(eval(args["eos"]))
+    processor_output_path = paths.processor_path(
+        processor_name=args["processor"],
+        dataset_year=args["year"],
+        label=args["label"],
+        mkdir=True,
+    )
+    return str(processor_output_path)
+
+
+def main(args):
+    args = vars(args)
+    submit = eval(args["submit"])
+    args["output_path"] = get_output_directories(args)
+    del args["label"]
+    del args["eos"]
+    del args["submit"]
+
+    # split dataset into batches
+    fileset_path = Path(f"{Path.cwd()}/analysis/fileset")
+    with open(f"{fileset_path}/fileset_{args['year']}_NANO_lxplus.json", "r") as f:
+        root_files = json.load(f)[args["dataset"]]
+    root_files_list = divide_list(root_files)
+
+    # submit job for each partition
+    for i, partition in enumerate(root_files_list, start=1):
+        if len(root_files_list) == 1:
+            args["dataset_key"] = args["dataset"]
+            args["partition_fileset"] = {args["dataset"]: partition}
+        else:
+            dataset_key = f'{args["dataset"]}_{i}'
+            args["nsample"] = i
+            args["dataset_key"] = dataset_key
+            args["partition_fileset"] = {dataset_key: partition}
+
+        submit_condor(args, submit=submit)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--processor",
+        dest="processor",
+        type=str,
+        default="ztojets",
+        help="processor to be used {ztojets}",
+    )
+    parser.add_argument(
+        "--dataset",
+        dest="dataset",
+        type=str,
+        default="",
+        help="sample key to be processed",
+    )
+    parser.add_argument(
+        "--year",
+        dest="year",
+        type=str,
+        default="2017",
+        help="year of the data {2016preVFP, 2016postVFP, 2017, 2018} (default 2017)",
+    )
+    parser.add_argument(
+        "--nfiles",
+        dest="nfiles",
+        type=int,
+        default=1,
+        help="number of .root files to be processed by sample. To run all files use -1 (default 1)",
+    )
+    parser.add_argument(
+        "--syst",
+        dest="syst",
+        type=str,
+        default="nominal",
+        help="systematic to apply {'nominal'}",
+    )
+    parser.add_argument(
+        "--nsample",
+        dest="nsample",
+        type=str,
+        default="",
+        help="partitions to run (--nsample 1,2,3 will only run partitions 1,2 and 3)",
+    )
+    parser.add_argument(
+        "--flow",
+        dest="flow",
+        type=str,
+        default="True",
+        help="whether to include underflow/overflow to first/last bin {True, False}",
+    )
+    parser.add_argument(
+        "--submit",
+        dest="submit",
+        type=str,
+        default="False",
+        help="if True submit job to Condor. If False, it just builds datasets and condor files",
+    )
+    parser.add_argument(
+        "--label",
+        dest="label",
+        type=str,
+        default="ZJets_CR",
+        help="Tag to recognize the run",
+    )
+    parser.add_argument(
+        "--eos",
+        dest="eos",
+        type=str,
+        default="False",
+        help="if True save outputs to /eos",
+    )
+    args = parser.parse_args()
+    main(args)
