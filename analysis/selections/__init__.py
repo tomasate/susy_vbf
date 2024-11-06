2347a2487e77b3f789987fd7d7958b510a0a573d
deoache <daniel.ocampoh@udea.edu.co>
2024-11-06 12:45:05 +0000

    update

--- analysis/selections/__init__.py
+++ analysis/selections/__init__.py
@@ -1,5 +1,5 @@
 from analysis.selections.utils import trigger_match
-from analysis.selections.object_selector import ObjectSelector
+from analysis.selections.object_selections import ObjectSelector
 import analysis.selections.event_selections as event_selections
 get_lumi_mask = event_selections.get_lumi_mask
 get_trigger_mask = event_selections.get_trigger_mask
