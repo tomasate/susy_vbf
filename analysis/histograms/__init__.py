e9d5841e1773de5a277ad422ee56675cf7147e50
deoache <daniel.ocampoh@udea.edu.co>
2024-11-25 09:05:19 +0000

    move 'histogram_config' to histogram folder

--- analysis/histograms/__init__.py
+++ analysis/histograms/__init__.py
@@ -1,2 +1,3 @@
 from analysis.histograms.hist_builder import HistBuilder
 from analysis.histograms.hist_filler import fill_histogram
+from analysis.histograms.histogram_config import VariableAxis, RegularAxis, IntCategoryAxis, StrCategoryAxis, HistogramConfig
