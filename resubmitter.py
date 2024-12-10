7292a3109189daf96ec1b9199ada5638eb3b7bb5
Tomas Atehortua <47373421+tomasate@users.noreply.github.com>
2024-12-10 10:25:53 +0100

    Update resubmitter to read the path from condor directives

--- resubmitter.py
+++ resubmitter.py
@@ -10,32 +10,51 @@ def main(args):
     """Helper function to resubmit condor jobs"""
 
     main_dir = Path.cwd()
-
-    # get outputs path
-    out_path = f"{args.output_path}/{args.processor}/{args.label}/{args.year}"
-    print(f"Reading outputs from: {out_path}")
-
-    # get datasets
-    fileset_path = main_dir / "analysis" / "filesets"
+    fileset_path = Path(f"{main_dir}/analysis/filesets")
     with open(f"{fileset_path}/{args.year}_fileset.yaml", "r") as f:
-        datasets = yaml.safe_load(f)
+        datasets = yaml.safe_load(f)#[f"{args.year}_UL"]
 
     # get jobs done
     jobs_done = []
+    out_path = f"{args.output_path}/{args.processor}"
+    if args.label:
+        out_path += f"/{args.label}"
+    if args.processor not in ["ztojets"]:
+        out_path += f"/{args.channel}/{args.lepton_flavor}"
+    if args.processor == "trigger_eff":
+        out_path += f"/{args.lepton_flavor}"
+    out_path += f"/{args.year}"
+    print(f"{out_path=}")
+    
     for sample in datasets:
         output_list = glob.glob(f"{out_path}/{sample}*.pkl")
         for output in output_list:
             jobs_done.append(output.split("/")[-1].replace(".pkl", ""))
     n_jobs_done = len(jobs_done)
-
+    
     # get jobs to be run
-    condor_path = main_dir / "condor" / args.processor / args.label / args.year
+    condor_path = f"{main_dir}/condor/{args.processor}"
+    if args.processor not in ["ztojets"]:
+        condor_path += f"/{args.channel}/{args.lepton_flavor}"
+    if args.processor == "trigger_eff":
+        condor_path += f"/{args.lepton_flavor}"
+    if args.label:
+        condor_path += f"/{args.label}"
+    condor_path += f"/{args.year}"
+    print(f"{condor_path=}")
     condor_files = glob.glob(f"{condor_path}/*/*.sub", recursive=True)
     n_jobs = len(condor_files)
-
+    
     # get jobs names
+    to_replace = f"{args.processor}_"
+    if args.processor not in ["ztojets"]:
+        to_replace += f"{args.channel}_{args.lepton_flavor}_"
+    if args.processor == "trigger_eff":
+        to_replace += f"{args.lepton_flavor}_"
     condor_files_keys = [
-        f.split("/")[-1].replace(f"{args.processor}_", "").replace(".sub", "")
+        f.split("/")[-1]
+        .replace(to_replace, "")
+        .replace(".sub", "")
         for f in condor_files
     ]
 
@@ -58,7 +77,7 @@ if __name__ == "__main__":
         "--output_path",
         dest="output_path",
         type=str,
-        # default="/eos/user/t/tatehort/susy_vbf/outs"
+        #default="/eos/user/t/tatehort/susy_vbf/outs"
         help="path to the 'outs' folder",
     )
     parser.add_argument(
@@ -72,15 +91,29 @@ if __name__ == "__main__":
         "--processor",
         dest="processor",
         type=str,
-        default="ztojets",
+        default="susy",
         help="processor to be used",
+    )
+    parser.add_argument(
+        "--channel",
+        dest="channel",
+        type=str,
+        default="",
+        help="channel to be processed {'2b1l', '1b1e1mu', '1b1l'}",
+    )
+    parser.add_argument(
+        "--lepton_flavor",
+        dest="lepton_flavor",
+        type=str,
+        default="",
+        help="lepton flavor to be processed {'mu', 'ele'}",
     )
     parser.add_argument(
         "--year",
         dest="year",
         type=str,
         default="2017",
-        help="year of the data {2016preVFP, 2016postVFP, 2017, 2018} (default 2017)",
+        help="year of the data {2016APV, 2016, 2017, 2018} (default 2017)",
     )
     parser.add_argument(
         "--resubmit",
