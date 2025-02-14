2bdf6795113969e13b972865f886d2a886c6c9c7
deoache <daniel.ocampoh@udea.edu.co>
2025-02-14 19:31:17 +0000

    update output directory definition

--- resubmitter.py
+++ resubmitter.py
@@ -1,14 +1,18 @@
-import json
 import glob
 import yaml
 import argparse
 import subprocess
 from pathlib import Path
+from analysis.helpers import get_output_directory
 
 
 def main(args):
     """Helper function to resubmit condor jobs"""
+    # get outputs directory
+    output_dir = get_output_directory(vars(args))
+    print(f"Reading outputs from: {output_dir}")
 
+    # get datasets key names
     main_dir = Path.cwd()
     fileset_path = Path(f"{main_dir}/analysis/filesets")
     with open(f"{fileset_path}/{args.year}_fileset.yaml", "r") as f:
@@ -16,77 +20,43 @@ def main(args):
 
     # get jobs done
     jobs_done = []
-    out_path = f"{args.output_path}/{args.processor}"
-    if args.label:
-        out_path += f"/{args.label}"
-    if args.processor not in ["ztojets"]:
-        out_path += f"/{args.channel}/{args.lepton_flavor}"
-    if args.processor == "trigger_eff":
-        out_path += f"/{args.lepton_flavor}"
-    out_path += f"/{args.year}"
-    print(f"{out_path=}")
-    
     for sample in datasets:
-        output_list = glob.glob(f"{out_path}/{sample}*.pkl")
+        output_list = glob.glob(f"{output_dir}/{sample}*.pkl")
         for output in output_list:
             jobs_done.append(output.split("/")[-1].replace(".pkl", ""))
     n_jobs_done = len(jobs_done)
-    
+
     # get jobs to be run
     condor_path = f"{main_dir}/condor/{args.processor}"
-    if args.processor not in ["ztojets"]:
-        condor_path += f"/{args.channel}/{args.lepton_flavor}"
-    if args.processor == "trigger_eff":
-        condor_path += f"/{args.lepton_flavor}"
     if args.label:
         condor_path += f"/{args.label}"
     condor_path += f"/{args.year}"
-    print(f"{condor_path=}")
+    print(f"Reading condor files from: {condor_path}")
     condor_files = glob.glob(f"{condor_path}/*/*.sub", recursive=True)
     n_jobs = len(condor_files)
-    
+
     # get jobs names
     to_replace = f"{args.processor}_"
-    if args.processor not in ["ztojets"]:
-        to_replace += f"{args.channel}_{args.lepton_flavor}_"
-    if args.processor == "trigger_eff":
-        to_replace += f"{args.lepton_flavor}_"
     condor_files_keys = [
-        f.split("/")[-1]
-        .replace(to_replace, "")
-        .replace(".sub", "")
+        f.split("/")[-1].replace(to_replace, "").replace(".sub", "")
         for f in condor_files
     ]
-
     for job, sub_file in zip(condor_files_keys, condor_files):
         if job not in jobs_done:
+            # missing job
             print(job)
-            if args.resubmit == "True":
-                # resubmit missing jobs
+            if args.resubmit:
+                # resubmit missing job
                 subprocess.run(["condor_submit", sub_file])
 
     print("")
     print(f"{n_jobs=}")
     print(f"{n_jobs_done=}")
-    print(f"missing files: {n_jobs - n_jobs_done}", "\n")
+    print(f"missing jobs: {n_jobs - n_jobs_done}", "\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--output_path",
-        dest="output_path",
-        type=str,
-        #default="/eos/user/t/tatehort/susy_vbf/outs",
-        help="path to the 'outs' folder",
-    )
-    parser.add_argument(
-        "--label",
-        dest="label",
-        type=str,
-        default="",
-        help="label of the run",
-    )
     parser.add_argument(
         "--processor",
         dest="processor",
@@ -102,11 +72,20 @@ if __name__ == "__main__":
         help="year of the data {2016preVFP, 2016postVFP, 2017, 2018} (default 2017)",
     )
     parser.add_argument(
-        "--resubmit",
-        dest="resubmit",
+        "--label",
+        dest="label",
         type=str,
-        default="False",
+        help="label of the run",
+    )
+    parser.add_argument(
+        "--resubmit",
+        action="store_true",
         help="if True resubmit the jobs. if False only print the missing jobs",
+    )
+    parser.add_argument(
+        "--eos",
+        action="store_true",
+        help="Enable reading outputs from /eos",
     )
     args = parser.parse_args()
     main(args)
